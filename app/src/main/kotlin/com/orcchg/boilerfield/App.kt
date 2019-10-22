899d83e3b8649d2f6b942345a12f67617e3388b8
Maxim Alov macbook <orcchg@gmail.com>
2019-10-22 10:37:27 +0300

    Set up build scripts and disable Crashlytics

--- app/src/main/kotlin/com/orcchg/boilerfield/App.kt
+++ app/src/main/kotlin/com/orcchg/boilerfield/App.kt
@@ -1,7 +1,13 @@
 package com.orcchg.boilerfield
 
 import android.app.Application
+import toothpick.Scope
+import toothpick.ktp.KTP
 
 class App : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+    }
 }
