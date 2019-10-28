ef194c328a16be4d5490cee403741ee8acfbc827
Maxim Alov macbook <orcchg@gmail.com>
2019-10-28 19:49:49 +0300

    Switch to use Dagger2 for DI

--- app/src/main/kotlin/com/orcchg/boilerfield/App.kt
+++ app/src/main/kotlin/com/orcchg/boilerfield/App.kt
@@ -1,13 +1,10 @@
 package com.orcchg.boilerfield
 
 import android.app.Application
-import toothpick.Scope
-import toothpick.ktp.KTP
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
     }
 }
